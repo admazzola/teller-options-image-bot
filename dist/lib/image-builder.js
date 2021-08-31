d85dcb68bcd6672e4a94e2bd8f3ff83614076b3e
Andy <admazzola@gmail.com>
2021-08-31 13:42:24 -0400

    adding gitignore

--- dist/lib/image-builder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ImageBuilder {
-    run() {
-        console.log('run image builder ');
-    }
-}
-exports.default = ImageBuilder;
-//# sourceMappingURL=image-builder.js.map
